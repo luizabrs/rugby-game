7457ab598065db893fe8b0613fd5aa55173c7679
Luiza Soezima <37429574+luizabrs@users.noreply.github.com>
2022-03-30 23:29:23 -0300

    attacker with strategy

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -19,11 +20,70 @@
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+  // UNUSED(attacker_position);
+  // UNUSED(defender_spy);
 
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+
+  // A partir de um número aleatório entre 1 e 9 o meu atacante executa algum movimento
+  // Se o numero aleatorio for 9, ele checa o spy e caso o 
+  int random = rand() % 9;
+
+  position_t current_defender_position;
+
+  direction_t new_direction = DIR_STAY;
+
+  if(random == 1){
+    new_direction = (direction_t) DIR_RIGHT;
+  }
+  else if(random == 2){
+    new_direction = (direction_t)  DIR_DOWN_RIGHT;
+  }
+  else if(random == 3){
+    new_direction = (direction_t) DIR_UP_RIGHT;
+  }
+  else if(random == 4){
+    new_direction = (direction_t) DIR_UP;
+  }
+  else if(random == 5){
+    new_direction = (direction_t) DIR_DOWN;
+  }
+  else if(random == 6){
+    new_direction = (direction_t) DIR_DOWN_LEFT;
+  }
+  else if(random == 7){
+    new_direction = (direction_t) DIR_UP_LEFT;
+  }
+  else if(random == 8){
+    new_direction = (direction_t) DIR_RIGHT;
+  }  
+  else if(random == 9){
+    // Se o numero aleatorio for 9, ele checa o spy    
+    current_defender_position= get_spy_position(defender_spy); 
+
+    // Se o defensor estiver em uma linha abaixo a do atacante, move para a diagonal superior direita em direção ao lado oposto
+    if(current_defender_position.i < attacker_position.i){
+      new_direction = (direction_t) DIR_UP_RIGHT;
+    }
+
+    // Se o defensor estiver na linha igual a do atacante, ele move o atacante para cima ou para baixo de forma pseudoaleatória
+    else  if(current_defender_position.i == attacker_position.i){
+      int another_random = rand() % 2;
+      if(another_random == 1){
+        new_direction = (direction_t) DIR_UP;
+      }
+      else{
+        new_direction = (direction_t) DIR_DOWN;
+      }
+    }
+
+    // Senão o atacante move para a diagonal inferior direita, sempre em direção ao lado oposto
+    else{
+      new_direction = (direction_t) DIR_DOWN_RIGHT;
+    }
+  }
+  return  new_direction;
+
 }
 
 /*----------------------------------------------------------------------------*/
